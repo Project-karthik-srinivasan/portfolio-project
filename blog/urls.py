4251edb0aa1c4d82bc64040eb7f3db7145f835fe
karthiks86 <karthiksrinivasan86@yahoo.com>
2018-10-13 18:00:51 -0500

    portfolio project with blog functionality and polish

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblog, name='allblog'),
+    path('', views.allblog, name='allblogs'),
+    path('<int:blog_id>/',views.detail, name='detail'),
 ]
